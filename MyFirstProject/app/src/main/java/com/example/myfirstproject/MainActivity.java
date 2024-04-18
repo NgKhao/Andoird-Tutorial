e9aa88a9a1d6cc5d01d7e244e9aa9111479ec749
NgKhao <bsbnk141@gmail.com>
2024-04-19 01:31:00 +0700

    add layout ctn

--- MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myfirstproject;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spinner, spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.layout_p2_bai1);
+//        gọi view sp2 cho spinner
+        spinner = findViewById(R.id.sp2);
+        String[] list = {"HUST", "PTIT", "NEU", "FTU"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.item, list);
+        spinner.setAdapter(adapter);
+
+        spinner2 = findViewById(R.id.sp1);
+        String[] list2 = getResources().getStringArray(R.array.country);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, R.layout.item, list2);
+        spinner2.setAdapter(adapter2);
     }
 }
