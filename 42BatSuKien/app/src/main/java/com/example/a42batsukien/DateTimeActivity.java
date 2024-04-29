9d4be046a006bf26d24c76c47176ec36612ec681
NgKhao <bsbnk141@gmail.com>
2024-04-29 15:37:32 +0700

    add menu

--- 42BatSuKien/app/src/main/java/com/example/a42batsukien/DateTimeActivity.java
+++ 42BatSuKien/app/src/main/java/com/example/a42batsukien/DateTimeActivity.java
@@ -2,14 +2,21 @@ package com.example.a42batsukien;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.graphics.Color;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,6 +28,8 @@ public class DateTimeActivity extends AppCompatActivity implements View.OnClickL
 
     private EditText et;
     private EditText ed;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +45,8 @@ public class DateTimeActivity extends AppCompatActivity implements View.OnClickL
         ed = findViewById(R.id.eDate);
         et.setOnClickListener(this);
         ed.setOnClickListener(this);
+        tv = findViewById(R.id.mMenu);
+        registerForContextMenu(tv); //nhấn và giữ sẽ hiển thị menu ngữ cảnh trên view tv
     }
 
     @Override
@@ -66,5 +77,47 @@ public class DateTimeActivity extends AppCompatActivity implements View.OnClickL
             }, y, m, d);
             datePickerDialog.show();
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        dùng để kích hoạt 1 menu tên main_menu vào Menu
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.mFile)
+            Toast.makeText(this, "File", Toast.LENGTH_SHORT).show();
+        else if(item.getItemId() == R.id.mEmail)
+            Toast.makeText(this, "Email", Toast.LENGTH_SHORT).show();
+        else if(item.getItemId() == R.id.mPhone)
+            Toast.makeText(this, "Phone", Toast.LENGTH_SHORT).show();
+        else if(item.getItemId() == R.id.mExit)
+            System.exit(0);
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.color_menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+            
+        if(id == R.id.mRed)
+            tv.setTextColor(Color.RED);
+        else if (id == R.id.mBlue)
+            tv.setTextColor(Color.BLUE);
+        else if(id == R.id.mYellow)
+            tv.setTextColor(Color.YELLOW);
+
+        return super.onContextItemSelected(item);
     }
 }
